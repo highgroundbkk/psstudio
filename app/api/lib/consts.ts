d46a5fa88700e1695fe103835328f78b4fba868a
Jakub <75627864+2xxn@users.noreply.github.com>
2025-12-03 15:48:48 +0100

    chore: fetch redirect uri from env

--- app/api/lib/consts.ts
+++ app/api/lib/consts.ts
@@ -3,5 +3,6 @@ export const BASE_URL = process.env.BASE_URL! || "https://streetviewpublish.goog
 
 export const X_CLIENT_ID = process.env.X_CLIENT_ID!
 export const X_CLIENT_SECRET = process.env.X_CLIENT_SECRET!
+export const REDIRECT_URI = process.env.REDIRECT_URI!;
 
 export const CBC_KEY = Buffer.from(process.env.CBC_KEY!, 'utf-8'); // 32 bytes for AES-256
--- app/api/lib/oauth.ts
+++ app/api/lib/oauth.ts
@@ -1,7 +1,7 @@
 import { OAuth2Client } from "google-auth-library";
-import { AUTH_SCOPE, X_CLIENT_ID, X_CLIENT_SECRET } from "./consts";
+import { AUTH_SCOPE, X_CLIENT_ID, X_CLIENT_SECRET, REDIRECT_URI } from "./consts";
 
-export const OAUTH2_CLIENT = new OAuth2Client(X_CLIENT_ID, X_CLIENT_SECRET, "https://photosphere.sowinski.tech/api/auth");
+export const OAUTH2_CLIENT = new OAuth2Client(X_CLIENT_ID, X_CLIENT_SECRET, REDIRECT_URI);
 
 export async function getOAuthToken() {
     const authorizeUrl = OAUTH2_CLIENT.generateAuthUrl({
